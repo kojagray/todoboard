723f41c5ba560f15663470e6f20ca23241543758
Jake <amiajokitou@gmail.com>
2023-01-13 13:36:08 -1000

    get_completed_tasks should not parametrize limit

--- todoist.py
+++ todoist.py
@@ -7,17 +7,20 @@ class Todoist:
         self.api_key = self._get_api_key()
         self.api = self._init_api()
 
+
     def _get_api_key(self):
         with open("data/apikey.txt", "r") as rp:
             api_key = rp.read()
             
             return api_key
 
+
     def _init_api(self):
         api = TodoistAPI(self.api_key)
         
         return api 
-        
+
+
     def get_all_projects(self):
             try:
                 projects = self.api.get_projects()
@@ -25,21 +28,25 @@ class Todoist:
             except Exception as error:
                 print(error)
 
-    def get_completed_tasks(self, num_tasks=200):
+
+    def get_completed_tasks(self, since=None):
         get_all_url = 'https://api.todoist.com/sync/v9/completed/get_all'
         headers = {'Authorization': 'Bearer {}'.format(self.api_key)}
+        
         params = {
-            "limit": num_tasks,
-            "annotate_notes": "true"
+            "annotate_notes": "true",
+            "limit": 200
         }
+        if since:
+            params["since"] = since
 
         try:
             response = requests.get(
                             get_all_url, 
                             headers=headers,
                             params=params)  
-            completed_tasks = response.json()
-            return completed_tasks['tems']
+            completed_tasks = response.json()['items']
+            return completed_tasks
         except Exception as error:
             print(error) 
             pass 
