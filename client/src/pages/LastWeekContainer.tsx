b050cf01b8a70c0075ac3290e980d11486f8f173
kojagray <amiajokitou@gmail.com>
2023-11-19 19:00:27 -1000

    added day of week to dates in last week task container

--- client/src/pages/LastWeekContainer.tsx
+++ client/src/pages/LastWeekContainer.tsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
+function getDayOfWeek(date: string) {
+    const dayOfWeek = new Date(date + " 00:00:00").getDay();
+    const weekday = isNaN(dayOfWeek) ? null :
+        ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'][dayOfWeek];
+
+    return `${date} (${weekday})`
+}
+
 function LastWeekContainer() {
     let [lastWeeksTasks, updateLastWeeksTasks] = useState<any[]>([]);
 
     useEffect(() => {
         fetch("http://localhost:8080/last_weeks_tasks")
-        .then((response) => response.json())
-        .then((data) => {
-            updateLastWeeksTasks( lastWeeksTasks => ({
-                ...lastWeeksTasks, ...data
-            }))
-        });
-        }, []);
-          
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                updateLastWeeksTasks(lastWeeksTasks => ({
+                    ...lastWeeksTasks, ...data
+                }))
+            });
+    }, []);
+
     return (
-        Object.keys(lastWeeksTasks).map((date) => 
+        Object.keys(lastWeeksTasks).map((date) =>
             <div className="card">
                 <div className="card-block">
-                    <h3 className="card-title">{date}</h3>
-                    {lastWeeksTasks[date as keyof typeof lastWeeksTasks].map((task: any) => 
+                    <h3 className="card-title">{getDayOfWeek(date)}</h3>
+                    {lastWeeksTasks[date as keyof typeof lastWeeksTasks].map((task: any) =>
                         <div className="card-text">{task.content}</div>
                     )}
                 </div>
